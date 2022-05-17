b5768e96a013fa77124ed8ce0437fabbe9ae2a94
Mayank Pandey <mayank.pandey@quincus.com>
2022-05-17 18:56:27 +0530

    Some changes

--- crud-with-routing/src/App.js
+++ crud-with-routing/src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
@@ -9,13 +8,11 @@ function App() {
   return (
     <div className="container" style={{ marginTop: '20px' }}>
       <Router>
-    		{/*
-        <ul>
+        	<ul>
     			<li><Link to="/">Home</Link></li>
     			<li><Link to="/profile">Profile</Link></li>
     			<li><Link to="/user/1">Edit User</Link></li>
     		</ul>
-        */}
     		<Switch>
     			<Route exact path="/" component={Home}></Route>
     			<Route exact path="/user/:id" component={EditUser}></Route>
